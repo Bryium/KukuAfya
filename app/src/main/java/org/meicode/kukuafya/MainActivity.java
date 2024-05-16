7c4d8f8dd59643dfca7e8cf22e1c68cbddfdb9b1
Bryium <bryiumonyancha@gmail.com>
2024-05-16 12:34:55 +0300

    added fragments functionality

--- app/src/main/java/org/meicode/kukuafya/MainActivity.java
+++ app/src/main/java/org/meicode/kukuafya/MainActivity.java
@@ -2,24 +2,65 @@ package org.meicode.kukuafya;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.meicode.kukuafya.MainActivity;
+import org.meicode.kukuafya.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+     ActivityMainBinding binding ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(bindng.getRoot);
+
+       replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.buttomNavigationView.setOnItemSelectedListener(item ->{
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.detect:
+                    replaceFragment(new DetectFragment());
+                    break;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.subscribe:
+                    replaceFragment(new SubscribeFragment());
+                    break;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.community:
+                    replaceFragment(new CommunityFragment());
+                    break;
+            }
+
+            return true;
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
 }
