23d89e9041d7a5a54b6b0d10aad9af18e51e20c5
Bryium <bryiumonyancha@gmail.com>
2024-05-18 23:20:08 +0300

    added the login function activity

--- app/src/main/java/org/meicode/kukuafya/LoginActivity.java
+++ app/src/main/java/org/meicode/kukuafya/LoginActivity.java
@@ -1,6 +1,10 @@
 package org.meicode.kukuafya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        TextView btn = findViewById(R.id.txtViewSignup);
+        Button loginButton = findViewById(R.id.btnlogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start MainActivity when login button is clicked
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
         });
     }
 }
