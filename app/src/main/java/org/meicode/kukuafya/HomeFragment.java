43632752e9b56d170123becc744ef863e698dcfd
Bryium <bryiumonyancha@gmail.com>
2024-05-28 00:23:50 +0300

    added  a webview to home fragment

--- app/src/main/java/org/meicode/kukuafya/HomeFragment.java
+++ app/src/main/java/org/meicode/kukuafya/HomeFragment.java
@@ -1,21 +1,27 @@
 package org.meicode.kukuafya;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class HomeFragment extends Fragment {
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        WebView webView = view.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.bentoli.com/chicken-problems-common/");
+
+        return view;
     }
 }
