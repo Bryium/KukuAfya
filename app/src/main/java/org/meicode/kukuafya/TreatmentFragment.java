ce118541afc15c3d5edb9e469f4b852aab09e8ed
Bryium <bryiumonyancha@gmail.com>
2024-07-13 17:31:49 +0300

    fixed the treatment fragment

--- app/src/main/java/org/meicode/kukuafya/TreatmentFragment.java
+++ app/src/main/java/org/meicode/kukuafya/TreatmentFragment.java
@@ -1,21 +1,39 @@
 package org.meicode.kukuafya;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class TreatmentFragment extends Fragment {
 
+    private static final String YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=nV7lk3lht60";
 
+    private WebView webView;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_treatment, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_treatment, container, false);
+
+        // Initialize WebView
+        webView = view.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+
+
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript (required for YouTube embeds)
+        webView.setWebViewClient(new WebViewClient()); // Open links in the WebView itself
+
+        // Load YouTube video
+        webView.loadUrl(YOUTUBE_VIDEO_URL);
+
+        return view;
     }
 }
