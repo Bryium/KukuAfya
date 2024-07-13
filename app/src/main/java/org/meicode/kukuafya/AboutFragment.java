65d10d19411522ee443d284f08f9bf8d86e42bd1
Bryium <bryiumonyancha@gmail.com>
2024-07-13 18:00:53 +0300

    fixed errors in about and treatment fragment

--- app/src/main/java/org/meicode/kukuafya/AboutFragment.java
+++ app/src/main/java/org/meicode/kukuafya/AboutFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
@@ -25,6 +26,25 @@ public class AboutFragment extends Fragment {
         String descriptionText = "Embark on an extraordinary journey with KukuAfya, a state-of-the-art Android application meticulously crafted with Java to revolutionize global chicken health management. Engineered for optimal efficiency and user-friendliness, KukuAfya seamlessly integrates cutting-edge Machine Learning components developed in Python to provide precise and timely disease diagnostics for livestock worldwide. With its intuitive interface, robust performance, and scalable architecture, KukuAfya empowers farmers, veterinarians, and agricultural enterprises with invaluable insights, ensuring proactive and sustainable animal healthcare management. Join us in harnessing the transformative power of technology with KukuAfya, setting new standards in livestock health and agriculture innovation on a global scale.";
 
         appDescription.setText(descriptionText);
+
+        // Find the NestedScrollView by ID and set the scroll change listener
+        NestedScrollView nestedScrollView = view.findViewById(R.id.nestedScrollView);
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY > oldScrollY) {
+                    // Scroll down, hide toolbar, bottomAppBar, and FAB
+                    ((MainActivity) getActivity()).hideToolbar();
+                    ((MainActivity) getActivity()).hideBottomAppBar();
+                    ((MainActivity) getActivity()).hideFloatingActionButton();
+                } else if (scrollY < oldScrollY) {
+                    // Scroll up, show toolbar, bottom navigation, and FAB
+                    ((MainActivity) getActivity()).showToolbar();
+                    ((MainActivity) getActivity()).showBottomAppBar();
+                    ((MainActivity) getActivity()).showFloatingActionButton();
+                }
+            }
+        });
 
         return view;
     }
