c7a8f3b5c2b4028dfb19436baeb0cdd145ade3e1
Bryium <bryiumonyancha@gmail.com>
2024-07-28 16:43:20 +0300

    fixed the error in detect fragment

--- app/src/main/java/org/meicode/kukuafya/DetectFragment.java
+++ app/src/main/java/org/meicode/kukuafya/DetectFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class DetectFragment extends Fragment {
+
+    Button selectBtn, predictBtn, captureBtn;
+
 
 
 
