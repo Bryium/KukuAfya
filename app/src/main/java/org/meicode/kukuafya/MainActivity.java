e059b5d741382ac8e8a3c945d932bc83977fb7da
Bryium <bryiumonyancha@gmail.com>
2025-07-05 21:43:12 +0300

    update the app about

--- app/src/main/java/org/meicode/kukuafya/MainActivity.java
+++ app/src/main/java/org/meicode/kukuafya/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -19,7 +18,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
